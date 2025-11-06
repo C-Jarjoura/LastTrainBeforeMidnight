b98efa94fdb94f4a99084c2deb60786805cc5c98
DysterFall <jarjourachristophe29052004@gmail.com>
2025-11-06 21:51:59 +0100

    Ajout de la classe abstraite Entity
    Création de la classe abstraite `Entity` dans `Entity.h` avec des méthodes virtuelles pures pour la mise à jour et le dessin.
    Ajout de méthodes pour gérer la position (`setPosition` et `getPosition`) et d'un membre protégé `m_position`.
    Ajout de la directive `#pragma once` pour éviter les inclusions multiples et inclusion de la bibliothèque SFML pour les fonctionnalités graphiques.
    Ajout d'un destructeur virtuel par défaut pour garantir une destruction correcte des classes dérivées.
    Mise à jour de `Entity.cpp` pour inclure `Entity.h` et ajout d'un commentaire explicatif.

---
+++ LastTrainBeforeMidnight/Entity.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+
+class Entity
+{
+public:
+    virtual ~Entity() = default;
+
+    virtual void update(float dt) = 0;
+    virtual void draw(sf::RenderWindow& window) = 0;
+
+    void setPosition(const sf::Vector2f& pos) { m_position = pos; }
+    sf::Vector2f getPosition() const { return m_position; }
+
+protected:
+    sf::Vector2f m_position;
+};
