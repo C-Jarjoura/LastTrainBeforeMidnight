de3032a237d7e71b84ec8d84e6cb02b1ddd10969
DysterFall <jarjourachristophe29052004@gmail.com>
2025-11-06 21:54:47 +0100

    Implémentation de la classe Player
    - Ajout de l'implémentation complète de la classe `Player` dans `Player.cpp`.
    - Suppression de `#pragma once` et ajout de `#include "Player.h"`.
    - Implémentation du constructeur `Player::Player()` :
      - Initialisation de la vitesse, de la texture, du sprite et de la position.
    - Ajout de la méthode `Player::update(float dt)` pour gérer les mouvements (Z, S, Q, D).
    - Ajout de la méthode `Player::draw(sf::RenderWindow& window)` pour dessiner le sprite.
    - Suppression des inclusions inutiles et des déclarations redondantes dans `Player.cpp`.

--- LastTrainBeforeMidnight/Player.cpp
+++ LastTrainBeforeMidnight/Player.cpp
@@ -1,18 +1,38 @@
-#pragma once
+#include "Player.h"
 
-#include <SFML/Graphics.hpp>
-#include "Entity.h"
-
-class Player : public Entity
+Player::Player()
 {
-public:
-    Player();
+    m_speed = 200.f;
 
-    void update(float dt) override;
-    void draw(sf::RenderWindow& window) override;
+    // Charge texture joueur (tu dois avoir un fichier player.png dans le dossier du .exe)
+    m_texture.loadFromFile("player.png");
+    m_sprite.setTexture(m_texture);
 
-private:
-    float m_speed;
-    sf::Sprite m_sprite;
-    sf::Texture m_texture;
-};
+    m_position = { 960.f, 540.f }; // centre écran 1920x1080
+    m_sprite.setPosition(m_position);
+}
+
+void Player::update(float dt)
+{
+    sf::Vector2f movement(0.f, 0.f);
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::W)) // Z (AZERTY)
+        movement.y -= m_speed * dt;
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::S))
+        movement.y += m_speed * dt;
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::A)) // Q (AZERTY)
+        movement.x -= m_speed * dt;
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::D))
+        movement.x += m_speed * dt;
+
+    m_position += movement;
+    m_sprite.setPosition(m_position);
+}
+
+void Player::draw(sf::RenderWindow& window)
+{
+    window.draw(m_sprite);
+}
